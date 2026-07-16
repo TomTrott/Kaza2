b91609550005c0cb467605ae5c8b0d3ee3818cb2
Tom ingremeau <tomingremeaumaillet@gmail.com>
2026-07-16 18:06:16 +0200

    commentaire controllerpreoperty test

--- Backend/tests/Unit/Controller/PropertyControllerTest.php
+++ Backend/tests/Unit/Controller/PropertyControllerTest.php
@@ -1,72 +1,42 @@
 <?php
-
-
 namespace App\Tests\Unit\Controller;
-
-
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-
-/**
- * Tests du contrôleur PropertyController.
- *
- * Vérifie que l'API retourne correctement
- * les propriétés utilisées par le frontend.
- */
 class PropertyControllerTest extends WebTestCase
 {
-
-    /**
-     * Test : L'endpoint GET /api/properties
-     * doit retourner une liste de propriétés.
-     */
+    /** L'endpoint GET /api/properties  doit retourner une liste de propriétés. */
     public function testListReturnsPropertiesForFrontend(): void
     {
-
         // Création d'un client Symfony
         $client = static::createClient();
-
 
         // Appel réel de l'API
         $client->request(
             'GET',
             '/api/properties'
         );
-
-
         // Vérifie que la réponse HTTP est correcte
         $this->assertResponseIsSuccessful();
-
-
         // Vérifie que la réponse est du JSON
         $this->assertResponseHeaderSame(
             'content-type',
             'application/json'
         );
-
-
         // Récupération des données JSON
         $data = json_decode(
             $client->getResponse()->getContent(),
             true
         );
-
-
         // Vérifie que la réponse est un tableau
         $this->assertIsArray($data);
     }
 
-
-    /**
-     * Test : Si l'API répond sans propriété,
-     * elle doit retourner un tableau JSON.
+    /** Si l'API répond sans propriété, elle doit retourner un tableau JSON.
      */
     public function testListReturnsArray(): void
     {
-
         // Création du client Symfony
         $client = static::createClient();
-
 
         // Appel de l'endpoint
         $client->request(
@@ -74,17 +44,14 @@ class PropertyControllerTest extends WebTestCase
             '/api/properties'
         );
 
-
         // Vérifie que Symfony répond correctement
         $this->assertResponseIsSuccessful();
-
 
         // Transforme la réponse JSON
         $data = json_decode(
             $client->getResponse()->getContent(),
             true
         );
-
 
         // Vérifie le format attendu par le frontend
         $this->assertIsArray($data);
